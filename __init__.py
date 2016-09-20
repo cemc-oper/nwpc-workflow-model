3ef18d8cdb95382db81b3512f210ce42a097df3e
perillaroc <perillaroc@gmail.com>
2016-09-20 15:17:04 +0800

    Add initial script for nwpc_log model.
    添加初始化数据库的脚本。

--- __init__.py
+++ __init__.py
@@ -2,5 +2,5 @@
 from nwpc_log.node import Node, NodeType
 from nwpc_log.bunch import Bunch
 from nwpc_log.node_status import NodeStatus
-from nwpc_log.models import RecordBase, Record, RepoVersion, SmsRepo, User, Model
+from nwpc_log.models import RecordBase, Record, RepoVersion, SmsRepo, Repo, User, Model
 
