6bca9d38ebd78552c6497bdd61497589f282f62a
perillaroc <perillaroc@gmail.com>
2016-09-23 10:21:17 +0800

    Move static method using database from npwc_log to nwpc_log_model.
    将与数据库有关的静态方法从 nwpc_log 移到 nwpc_log_model 中。

--- bunch.py
+++ bunch.py
@@ -1,8 +1,5 @@
 # coding=utf-8
-from sqlalchemy import distinct
-
 from nwpc_log.node import Node
-from nwpc_log_model.models import Record
 
 
 class Bunch(Node):
@@ -75,27 +72,3 @@ class Bunch(Node):
                 return None
             cur_node = t_node
         return cur_node
-
-    @staticmethod
-    def generate_repo_tree_from_session(owner, repo, query_date, session):
-        """
-        从数据库中某天的日志条目生成Bunch。
-
-        :param owner: repo owner
-        :param repo: repo name
-        :param query_date: some day
-        :param session: sql db connection session for
-        :return: bunch object in dict.
-        """
-        Record.prepare(owner, repo)
-        # get node list
-        query = session.query(distinct(Record.record_fullname)) \
-            .filter(Record.record_date == query_date) \
-            .filter(Record.record_fullname.isnot(None)) \
-            .order_by(Record.record_fullname)
-        records = query.all()
-        bunch = Bunch()
-        for a_node_path in records:
-            bunch.add_node(a_node_path[0])
-        # tree = bunch.to_dict()
-        return bunch
--- node.py
+++ node.py
@@ -1,5 +1,4 @@
 # coding=utf-8
-from nwpc_log_model.models import Record
 
 
 class NodeType(object):
@@ -118,51 +117,3 @@ class Node(object):
 
     def get_node_type_string(self):
         return NodeType.get_node_type_string(self.get_node_type())
-
-    @staticmethod
-    def node_is_valid_from_session(node_path, session):
-        # TODO (windroc, 2015.05.15): 何时按照 owner/repo 准备 Record 对象。
-        query = session.query(Record).filter(Record.record_fullname == node_path).limit(1).all()
-        if len(query) == 0:
-            return False
-        else:
-            return True
-
-    @staticmethod
-    def sub_node_is_valid_from_session(node_path, session):
-        # NOTE: 花费太长时间，最好由 Spark 任务得到节点树后查找是否有子节点。
-        path = node_path+'/'
-        query = session.query(Record.record_fullname) \
-            .filter("LEFT(record_fullname, {path_length}) = '{path}'".format(path_length=len(path), path=path))\
-            .filter("LEFT(record_fullname, {alias_path_length}) != '{alias_path}'".format(
-                alias_path_length=len(path+'alias'), alias_path=path+'alias'))\
-            .limit(1)
-        print query
-        query = query.all()
-        if len(query) == 0:
-            return False
-        else:
-            return True
-
-    @staticmethod
-    def get_node_type_from_session(node_path, session):
-        # NOTE：花费时间太长，最好由 Spark 任务得到节点树后再确定节点类型。
-        node_type = NodeType.get_node_type_string(NodeType.Unknown)
-        if node_path == '/':
-            return NodeType.get_node_type_string(NodeType.Root)
-        if not node_path.startswith('/'):
-            return node_type
-        # test whether the node is exists
-        if not Node.node_is_valid_from_session(node_path, session):
-            return node_type
-
-        if node_path.find('/', 1) == -1:
-            return NodeType.get_node_type_string(NodeType.Suite)
-
-        if Node.sub_node_is_valid_from_session(node_path, session):
-            return NodeType.get_node_type_string(NodeType.Family)
-        else:
-            if node_path.find(":") != -1:
-                return NodeType.get_node_type_string(NodeType.NonTaskNode)
-            else:
-                return NodeType.get_node_type_string(NodeType.Task)
