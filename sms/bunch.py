4854342c4ef9c411de09a49d777e987968024ef9
perillaroc <perillaroc@gmail.com>
2017-01-10 15:07:48 +0800

    use add_node in add_node_status. And add unit test for bunch.

--- sms/bunch.py
+++ sms/bunch.py
@@ -10,7 +10,7 @@ class Bunch(Node):
         self.parent = None
         self.children = list()
         self.name = ''
-        self.status = 'unknown'
+        self.status = 'unk'
 
     @staticmethod
     def create_from_dict(node_dict, parent=None):
@@ -63,32 +63,36 @@ class Bunch(Node):
         node_status = node_status_object['status']
         node_name = node_status_object['name']
 
-        if node_path == '/':
-            self.status = node_status
-            return self
-
-        node = None
-        if node_path[0] != '/':
-            return node
-
-        node_path = node_path[1:]
-        tokens = node_path.split("/")
-        cur_node = self
-        for a_token in tokens:
-            t_node = None
-            for a_child in cur_node.children:
-                if a_child.name == a_token:
-                    t_node = a_child
-                    break
-            if t_node is None:
-                t_node = Node()
-                t_node.parent = cur_node
-                t_node.name = node_name
-                t_node.status = node_status
-                t_node.children = list()
-                cur_node.add_child(t_node)
-            cur_node = t_node
-        return cur_node
+        # if node_path == '/':
+        #     self.status = node_status
+        #     return self
+        #
+        # node = None
+        # if node_path[0] != '/':
+        #     return node
+        #
+        # node_path = node_path[1:]
+        # tokens = node_path.split("/")
+        # cur_node = self
+        # for a_token in tokens:
+        #     t_node = None
+        #     for a_child in cur_node.children:
+        #         if a_child.name == a_token:
+        #             t_node = a_child
+        #             break
+        #     if t_node is None:
+        #         t_node = Node()
+        #         t_node.parent = cur_node
+        #         t_node.name = node_name
+        #         t_node.status = node_status
+        #         t_node.children = list()
+        #         cur_node.add_child(t_node)
+        #     cur_node = t_node
+        # return cur_node
+        node = self.add_node(node_path)
+        node.status = node_status
+        node.name = node_name
+        return node
 
     def find_node(self, node_path):
         if node_path == '/':
