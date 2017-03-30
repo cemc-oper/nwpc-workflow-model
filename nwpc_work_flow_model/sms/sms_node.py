11f5c8e82de4e6bd42ebe4a07786f1fd8957d70d
perillaroc <perillaroc@gmail.com>
2017-03-30 09:15:10 +0800

    move get_sms_node_from_cdp_output to SmsNode.

--- nwpc_work_flow_model/sms/sms_node.py
+++ nwpc_work_flow_model/sms/sms_node.py
@@ -9,6 +9,93 @@ class SmsNode(object):
         self.status = None
         self.node_type = None
 
+    @staticmethod
+    def create_from_cdp_output(cdp_output):
+        # find status
+        cur_line_no = 0
+        line_count = len(cdp_output)
+
+        while cur_line_no < line_count:
+            line = cdp_output[cur_line_no]
+            if len(line) > 0 and line.startswith('/') and not line.startswith('/-CDP'):
+                break
+            cur_line_no += 1
+        if cur_line_no == line_count:
+            return None
+
+        # find show
+        while cur_line_no < line_count:
+            line = cdp_output[cur_line_no]
+            if line == '\n':
+                break
+            cur_line_no += 1
+
+        cur_line_no += 1
+        if cur_line_no >= line_count:
+            return None
+
+        line = cdp_output[cur_line_no]
+        if line[0].startswith('#'):
+            return None
+
+        start_line_no = cur_line_no
+
+        cur_line_no += 1
+        end_line_no = -1
+        while cur_line_no < line_count:
+            line = cdp_output[cur_line_no]
+            if not line.startswith('  '):
+                end_line_no = cur_line_no
+                break
+            cur_line_no += 1
+
+        if end_line_no == -1:
+            return None
+
+        # get variables
+        node_line = cdp_output[start_line_no: end_line_no]
+        node = SmsNode()
+        tokens = node_line[0].strip().split(' ')
+        if len(tokens) > 5:
+            node.node_type = tokens[0]
+            node.name = tokens[1]
+            node.status = tokens[4]
+
+        for line in node_line[1:]:
+            line = line.strip()
+            if line.startswith('# genvar '):
+                index = line.find(' ', 9)
+                if index == -1:
+                    continue
+                variable_name = line[9:index]
+                variable_value = line[index + 1:]
+                if variable_value[0] == '\'' and variable_value[-1] == '\'':
+                    variable_value = variable_value[1:-1]
+                variable = SmsNodeVariable(
+                    SmsNodeVariableType.GeneratedVariable,
+                    variable_name,
+                    variable_value
+                )
+                node.generated_variable_list.append(variable)
+            elif line.startswith('edit '):
+                index = line.find(' ', 5)
+                if index == -1:
+                    continue
+                variable_name = line[5:index]
+                variable_value = line[index + 1:]
+                if variable_value[0] == '\'' and variable_value[-1] == '\'':
+                    variable_value = variable_value[1:-1]
+                variable = SmsNodeVariable(
+                    SmsNodeVariableType.Variable,
+                    variable_name,
+                    variable_value
+                )
+                node.variable_list.append(variable)
+            else:
+                pass
+
+        return node
+
     def to_dict(self):
         return {
             'name': self.name,
@@ -33,90 +120,3 @@ class SmsNode(object):
             return None
         else:
             return var.value
-
-
-def get_sms_node_from_cdp_output(cdp_output):
-    # find status
-    cur_line_no = 0
-    line_count = len(cdp_output)
-
-    while cur_line_no < line_count:
-        line = cdp_output[cur_line_no]
-        if len(line) > 0 and line.startswith('/') and not line.startswith('/-CDP'):
-            break
-        cur_line_no += 1
-    if cur_line_no == line_count:
-        return None
-
-    # find show
-    while cur_line_no < line_count:
-        line = cdp_output[cur_line_no]
-        if line == '\n':
-            break
-        cur_line_no += 1
-
-    cur_line_no += 1
-    if cur_line_no >= line_count:
-        return None
-
-    line = cdp_output[cur_line_no]
-    if line[0].startswith('#'):
-        return None
-
-    start_line_no = cur_line_no
-
-    cur_line_no += 1
-    end_line_no = -1
-    while cur_line_no < line_count:
-        line = cdp_output[cur_line_no]
-        if not line.startswith('  '):
-            end_line_no = cur_line_no
-            break
-        cur_line_no += 1
-
-    if end_line_no == -1:
-        return None
-
-    # get variables
-    node_line = cdp_output[start_line_no: end_line_no]
-    node = SmsNode()
-    tokens = node_line[0].strip().split(' ')
-    if len(tokens) > 5:
-        node.node_type = tokens[0]
-        node.name = tokens[1]
-        node.status = tokens[4]
-
-    for line in node_line[1:]:
-        line = line.strip()
-        if line.startswith('# genvar '):
-            index = line.find(' ', 9)
-            if index == -1:
-                continue
-            variable_name = line[9:index]
-            variable_value = line[index+1:]
-            if variable_value[0] == '\'' and variable_value[-1] == '\'':
-                variable_value = variable_value[1:-1]
-            variable = SmsNodeVariable(
-                SmsNodeVariableType.GeneratedVariable,
-                variable_name,
-                variable_value
-            )
-            node.generated_variable_list.append(variable)
-        elif line.startswith('edit '):
-            index = line.find(' ', 5)
-            if index == -1:
-                continue
-            variable_name = line[5:index]
-            variable_value = line[index + 1:]
-            if variable_value[0] == '\'' and variable_value[-1] == '\'':
-                variable_value = variable_value[1:-1]
-            variable = SmsNodeVariable(
-                SmsNodeVariableType.Variable,
-                variable_name,
-                variable_value
-            )
-            node.variable_list.append(variable)
-        else:
-            pass
-
-    return node
--- tests/sms/test_sms_node_variable.py
+++ tests/sms/test_sms_node_variable.py
@@ -1,12 +1,12 @@
 import os
-from nwpc_work_flow_model.sms.sms_node import get_sms_node_from_cdp_output
+from nwpc_work_flow_model.sms.sms_node import SmsNode
 
 
 class TestSmsNode(object):
     def test_normal_suite_cdp_output(self):
         with open(os.path.dirname(__file__) + "/data/cdp/normal_suite_cdp.txt") as f:
             cdp_output = f.readlines()
-            node = get_sms_node_from_cdp_output(cdp_output)
+            node = SmsNode.create_from_cdp_output(cdp_output)
             assert node.get_variable_value('SUITE') == 'grapes_meso_v4_1'
             assert node.get_variable_value('DATE') == '15.01.2017'
             assert node.get_variable_value('DAY') == 'sunday'
@@ -19,7 +19,7 @@ class TestSmsNode(object):
     def test_normal_family_cdp_output(self):
         with open(os.path.dirname(__file__) + "/data/cdp/normal_family_cdp.txt") as f:
             cdp_output = f.readlines()
-            node = get_sms_node_from_cdp_output(cdp_output)
+            node = SmsNode.create_from_cdp_output(cdp_output)
             assert node.get_variable_value('FAMILY') == 'cold'
             assert node.get_variable_value('FAMILY1') == 'cold'
 
@@ -29,17 +29,17 @@ class TestSmsNode(object):
     def test_error_login_cdp(self):
         with open(os.path.dirname(__file__) + "/data/cdp/error_login_cdp.txt") as f:
             cdp_output = f.readlines()
-            node = get_sms_node_from_cdp_output(cdp_output)
+            node = SmsNode.create_from_cdp_output(cdp_output)
             assert node is None
 
     def test_error_node_path_cdp(self):
         with open(os.path.dirname(__file__) + "/data/cdp/error_node_path_cdp.txt") as f:
             cdp_output = f.readlines()
-            node = get_sms_node_from_cdp_output(cdp_output)
+            node = SmsNode.create_from_cdp_output(cdp_output)
             assert node is None
 
     def test_error_sms_server_cdp(self):
         with open(os.path.dirname(__file__) + "/data/cdp/error_sms_server_cdp.txt") as f:
             cdp_output = f.readlines()
-            node = get_sms_node_from_cdp_output(cdp_output)
+            node = SmsNode.create_from_cdp_output(cdp_output)
             assert node is None
