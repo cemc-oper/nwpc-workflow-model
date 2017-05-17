01f29c3f18f5d8f99becc5b28e8f752d95a6ab28
perillaroc <perillaroc@gmail.com>
2017-05-17 10:15:23 +0800

    move cdp test data to show directory.

--- tests/sms/test_sms_node.py
+++ tests/sms/test_sms_node.py
@@ -7,7 +7,7 @@ from nwpc_work_flow_model.sms.node_variable import SmsNodeVariableType
 
 class TestSmsNode(object):
     def test_normal_suite_cdp_output(self):
-        with open(os.path.dirname(__file__) + "/data/cdp/normal_suite_cdp.txt") as f:
+        with open(os.path.dirname(__file__) + "/data/cdp/show/normal_suite_cdp.txt") as f:
             cdp_output = f.readlines()
             node = SmsNode.create_from_cdp_output(cdp_output)
             assert node.get_variable_value('SUITE') == 'grapes_meso_v4_1'
@@ -20,7 +20,7 @@ class TestSmsNode(object):
             assert node.get_variable_value('VERSION') == '_v4_1'
 
     def test_normal_family_cdp_output(self):
-        with open(os.path.dirname(__file__) + "/data/cdp/normal_family_cdp.txt") as f:
+        with open(os.path.dirname(__file__) + "/data/cdp/show/normal_family_cdp.txt") as f:
             cdp_output = f.readlines()
             node = SmsNode.create_from_cdp_output(cdp_output)
             assert node.get_variable_value('FAMILY') == 'cold'
@@ -30,19 +30,19 @@ class TestSmsNode(object):
             assert node.get_variable_value('SMSFILES') == '/cma/u/nwp/smsworks/def/grapes_meso/smsfiles'
 
     def test_error_login_cdp(self):
-        with open(os.path.dirname(__file__) + "/data/cdp/error_login_cdp.txt") as f:
+        with open(os.path.dirname(__file__) + "/data/cdp/show/error_login_cdp.txt") as f:
             cdp_output = f.readlines()
             node = SmsNode.create_from_cdp_output(cdp_output)
             assert node is None
 
     def test_error_node_path_cdp(self):
-        with open(os.path.dirname(__file__) + "/data/cdp/error_node_path_cdp.txt") as f:
+        with open(os.path.dirname(__file__) + "/data/cdp/show/error_node_path_cdp.txt") as f:
             cdp_output = f.readlines()
             node = SmsNode.create_from_cdp_output(cdp_output)
             assert node is None
 
     def test_error_sms_server_cdp(self):
-        with open(os.path.dirname(__file__) + "/data/cdp/error_sms_server_cdp.txt") as f:
+        with open(os.path.dirname(__file__) + "/data/cdp/show/error_sms_server_cdp.txt") as f:
             cdp_output = f.readlines()
             node = SmsNode.create_from_cdp_output(cdp_output)
             assert node is None
