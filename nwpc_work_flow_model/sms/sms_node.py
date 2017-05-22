916b9a6162227bcc832b42f64f08f82ac5e9b44b
perillaroc <perillaroc@gmail.com>
2017-05-22 09:47:37 +0800

    add inherited_variable_list in SmsNode. #1

--- nwpc_work_flow_model/sms/sms_node.py
+++ nwpc_work_flow_model/sms/sms_node.py
@@ -6,6 +6,17 @@ class SmsNode(object):
     def __init__(self):
         self.variable_list = []
         self.generated_variable_list = []
+
+        """
+        inherited_variable_list, an array of objects as follows:
+            {
+                'path': node_path,
+                'variable_list': variable list,
+                'generated_variable_list': generated variable list
+            }
+        """
+        self.inherited_variable_list = []
+
         self.name = ''
         self.path = None
         self.status = None
@@ -99,12 +110,20 @@ class SmsNode(object):
         return node
 
     def to_dict(self):
+        inherited_variable_list = []
+        for node in self.inherited_variable_list:
+            inherited_variable_list.append({
+                'path': node['path'],
+                'variable_list': [var.to_dict() for var in node['variable_list']],
+                'generated_variable_list': [var.to_dict() for var in node['generated_variable_list']]
+            })
         return {
             'name': self.name,
             'status': self.status,
             'node_type': self.node_type,
             'variable_list': [var.to_dict() for var in self.variable_list],
-            'generated_variable_list': [var.to_dict() for var in self.generated_variable_list]
+            'generated_variable_list': [var.to_dict() for var in self.generated_variable_list],
+            'inherited_variable_list': inherited_variable_list
         }
 
     @staticmethod
@@ -119,6 +138,16 @@ class SmsNode(object):
         for a_var_dict in node_dict['generated_variable_list']:
             a_var = SmsNodeVariable.create_from_dict(a_var_dict)
             node.generated_variable_list.append(a_var)
+
+        for a_parent in node_dict['inherited_variable_list']:
+            node.inherited_variable_list.append({
+                'path': a_parent['path'],
+                'variable_list': [SmsNodeVariable.create_from_dict(a_var_dict)
+                                  for a_var_dict in a_parent['variable_list']],
+                'generated_variable_list': [SmsNodeVariable.create_from_dict(a_var_dict)
+                                  for a_var_dict in a_parent['generated_variable_list']]
+            })
+
         return node
 
     def get_variable(self, variable_name):
@@ -128,6 +157,14 @@ class SmsNode(object):
         for var in self.generated_variable_list:
             if var.name == variable_name:
                 return var
+
+        for node in self.inherited_variable_list:
+            for var in node['variable_list']:
+                if var.name == variable_name:
+                    return var
+            for var in node['generated_variable_list']:
+                if var.name == variable_name:
+                    return var
         return None
 
     def get_variable_value(self, variable_name):
--- tests/sms/test_sms_node.py
+++ tests/sms/test_sms_node.py
@@ -6,11 +6,12 @@ from nwpc_work_flow_model.sms.node_variable import SmsNodeVariableType, SmsNodeV
 
 
 class TestSmsNode(object):
-    def test_node_variable(self):
+    def create_node(self):
         node = SmsNode()
         node.variable_list.extend([
             SmsNodeVariable(SmsNodeVariableType.Variable, 'self_var_1', 'self_var_1_value'),
-            SmsNodeVariable(SmsNodeVariableType.Variable, 'var', 'var1')
+            SmsNodeVariable(SmsNodeVariableType.Variable, 'var', 'var1'),
+            SmsNodeVariable(SmsNodeVariableType.Variable, 'father_var_2', 'father_var_2_in_node'),
         ])
 
         node.generated_variable_list.extend([
@@ -18,10 +19,49 @@ class TestSmsNode(object):
             SmsNodeVariable(SmsNodeVariableType.GeneratedVariable, 'var', 'var2')
         ])
 
-        assert node.get_variable('self_var_1') == 'self_var_1_value'
-        assert node.get_variable('self_gen_var_1') == 'self_gen_var_1_value'
-        assert node.get_variable('var') == 'var1'
+        # root_node -> father_node -> node
+        node.inherited_variable_list.extend([
+            {
+                'path': '/root/father',
+                'variable_list': [
+                    SmsNodeVariable(SmsNodeVariableType.Variable, 'father_var', 'father_var_value'),
+                    SmsNodeVariable(SmsNodeVariableType.Variable, 'father_var_2', 'father_var_2_value'),
+                    SmsNodeVariable(SmsNodeVariableType.Variable, 'root_var_2', 'root_var_2_value_in_father')
+                ],
+                'generated_variable_list': [
+                    SmsNodeVariable(SmsNodeVariableType.Variable, 'father_gen_var', 'father_gen_var_value')
+                ]
+            },
+            {
+                'path': '/root',
+                'variable_list': [
+                    SmsNodeVariable(SmsNodeVariableType.Variable, 'root_var', 'root_var_value'),
+                    SmsNodeVariable(SmsNodeVariableType.Variable, 'root_var_2', 'root_var_2_value')
+                ],
+                'generated_variable_list': [
+                    SmsNodeVariable(SmsNodeVariableType.Variable, 'root_gen_var', 'root_gen_var_value')
+                ]
+            }
+        ])
+        return node
 
+    def test_node_variable(self):
+        node = self.create_node()
+
+        # node's variables
+        assert node.get_variable_value('self_var_1') == 'self_var_1_value'
+        assert node.get_variable_value('self_gen_var_1') == 'self_gen_var_1_value'
+
+        # Variable type overrides generated variable type.
+        assert node.get_variable_value('var') == 'var1'
+        assert node.get_variable('var').variable_type == SmsNodeVariableType.Variable
+
+        # inherited variables
+        assert node.get_variable_value('father_var') == 'father_var_value'
+        assert node.get_variable_value('root_var') == 'root_var_value'
+
+        assert node.get_variable_value('father_var_2') == 'father_var_2_in_node'
+        assert node.get_variable_value('root_var_2') == 'root_var_2_value_in_father'
 
     def test_show_normal_suite_cdp_output(self):
         with open(os.path.dirname(__file__) + "/data/cdp/show/normal_suite_cdp.txt") as f:
@@ -64,6 +104,14 @@ class TestSmsNode(object):
             node = SmsNode.create_from_cdp_show_output(cdp_output)
             assert node is None
 
+    def test_to_dict(self):
+        node = self.create_node()
+        node_dict = node.to_dict()
+
+        assert len(node_dict['variable_list']) == 3
+        assert len(node_dict['generated_variable_list']) == 2
+        assert len(node_dict['inherited_variable_list']) == 2
+
     def test_create_from_dict(self):
         node_dict = {
             'name': 'node name',
@@ -82,6 +130,42 @@ class TestSmsNode(object):
                     'variable_type': SmsNodeVariableType.GeneratedVariable,
                     'value': 'gen var value'
                 }
+            ],
+            'inherited_variable_list': [
+                {
+                    'path': '/root/father',
+                    'variable_list': [
+                        {
+                            'name': 'father var name',
+                            'variable_type': SmsNodeVariableType.Variable,
+                            'value': 'father var value'
+                        }
+                    ],
+                    'generated_variable_list': [
+                        {
+                            'name': 'father gen var name',
+                            'variable_type': SmsNodeVariableType.GeneratedVariable,
+                            'value': 'father gen var value'
+                        }
+                    ]
+                },
+                {
+                    'path': '/root',
+                    'variable_list': [
+                        {
+                            'name': 'root var name',
+                            'variable_type': SmsNodeVariableType.Variable,
+                            'value': 'root var value'
+                        }
+                    ],
+                    'generated_variable_list': [
+                        {
+                            'name': 'root gen var name',
+                            'variable_type': SmsNodeVariableType.GeneratedVariable,
+                            'value': 'root gen var value'
+                        }
+                    ]
+                }
             ]
         }
 
@@ -92,6 +176,7 @@ class TestSmsNode(object):
         assert node.node_type == node_dict['node_type']
         assert len(node.variable_list) == 1
         assert len(node.generated_variable_list) == 1
+        assert len(node.inherited_variable_list) == 2
 
     def test_info_normal_suite_cdp_output(self):
         with open(os.path.dirname(__file__) + "/data/cdp/info/info_normal_suite.txt") as f:
