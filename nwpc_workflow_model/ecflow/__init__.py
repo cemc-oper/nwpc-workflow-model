07dc61f23b93f1d20cca307c9726642017dba4ce
perillaroc <perillaroc@gmail.com>
2018-07-10 15:18:53 +0800

    add error status visitor for ecflow.

--- nwpc_workflow_model/ecflow/__init__.py
+++ nwpc_workflow_model/ecflow/__init__.py
@@ -1,8 +1,9 @@
 # coding=utf-8
 from nwpc_workflow_model import NodeVisitor, SimplePrintVisitor, \
-    pre_order_travel, ErrorStatusTaskVisitor
+    pre_order_travel
 
 from .node import Node
 from .node_type import NodeType
 from .bunch import Bunch
 from .node_status import NodeStatus
+from .visitor import ErrorStatusTaskVisitor
