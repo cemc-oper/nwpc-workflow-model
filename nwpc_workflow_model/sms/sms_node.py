fd71868d44f77e08f0058eba2157a8b8575843af
perillaroc <perillaroc@gmail.com>
2019-01-03 16:30:39 +0800

    :recycle: combine SmsNode and EcflowNode. #5

--- nwpc_workflow_model/sms/sms_node.py
+++ nwpc_workflow_model/sms/sms_node.py
@@ -1,29 +1,14 @@
 # coding: utf-8
+from nwpc_workflow_model.workflow_node import WorkflowNode
 from nwpc_workflow_model.node_variable import NodeVariable, NodeVariableType
 
 
-class SmsNode(object):
+class SmsNode(WorkflowNode):
     def __init__(self):
-        self.variable_list = []
-        self.generated_variable_list = []
+        WorkflowNode.__init__(self)
 
-        """
-        inherited_variable_list, an array of objects as follows:
-            {
-                'path': node_path,
-                'variable_list': Variable list,
-                'generated_variable_list': generated Variable list
-            }
-        """
-        self.inherited_variable_list = []
-
-        self.name = ''
-        self.path = None
-        self.status = None
-        self.node_type = None
-
-    @staticmethod
-    def create_from_cdp_show_output(cdp_output):
+    @classmethod
+    def create_from_cdp_show_output(cls, cdp_output):
         # find status
         cur_line_no = 0
         line_count = len(cdp_output)
@@ -109,8 +94,8 @@ class SmsNode(object):
 
         return node
 
-    @staticmethod
-    def create_from_cdp_info_output(cdp_output):
+    @classmethod
+    def create_from_cdp_info_output(cls, cdp_output):
         # find status
         cur_line_no = 0
         line_count = len(cdp_output)
@@ -194,68 +179,3 @@ class SmsNode(object):
                     node.inherited_variable_list[-1]['generated_variable_list'].append(variable)
 
         return node
-
-    def to_dict(self):
-        inherited_variable_list = []
-        for node in self.inherited_variable_list:
-            inherited_variable_list.append({
-                'path': node['path'],
-                'variable_list': [var.to_dict() for var in node['variable_list']],
-                'generated_variable_list': [var.to_dict() for var in node['generated_variable_list']]
-            })
-        return {
-            'name': self.name,
-            'status': self.status,
-            'node_type': self.node_type,
-            'variable_list': [var.to_dict() for var in self.variable_list],
-            'generated_variable_list': [var.to_dict() for var in self.generated_variable_list],
-            'inherited_variable_list': inherited_variable_list
-        }
-
-    @staticmethod
-    def create_from_dict(node_dict):
-        node = SmsNode()
-        node.name = node_dict['name']
-        node.status = node_dict['status']
-        node.node_type = node_dict['node_type']
-        for a_var_dict in node_dict['variable_list']:
-            a_var = NodeVariable.create_from_dict(a_var_dict)
-            node.variable_list.append(a_var)
-        for a_var_dict in node_dict['generated_variable_list']:
-            a_var = NodeVariable.create_from_dict(a_var_dict)
-            node.generated_variable_list.append(a_var)
-
-        for a_parent in node_dict['inherited_variable_list']:
-            node.inherited_variable_list.append({
-                'path': a_parent['path'],
-                'variable_list': [NodeVariable.create_from_dict(a_var_dict)
-                                  for a_var_dict in a_parent['variable_list']],
-                'generated_variable_list': [NodeVariable.create_from_dict(a_var_dict)
-                                            for a_var_dict in a_parent['generated_variable_list']]
-            })
-
-        return node
-
-    def get_variable(self, variable_name):
-        for var in self.variable_list:
-            if var.name == variable_name:
-                return var
-        for var in self.generated_variable_list:
-            if var.name == variable_name:
-                return var
-
-        for node in self.inherited_variable_list:
-            for var in node['variable_list']:
-                if var.name == variable_name:
-                    return var
-            for var in node['generated_variable_list']:
-                if var.name == variable_name:
-                    return var
-        return None
-
-    def get_variable_value(self, variable_name):
-        var = self.get_variable(variable_name)
-        if var is None:
-            return None
-        else:
-            return var.value
